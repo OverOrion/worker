ce98439e26034c65cf598eab824ad500d9145d21
Szilard Parrag <szilard.parrag@gmail.com>
2022-09-23 16:15:15 +0200

    removed constants
    Signed-off-by: Szilard Parrag <szilard.parrag@gmail.com>

--- app-libs/sgx-runtime/src/lib.rs
+++ app-libs/sgx-runtime/src/lib.rs
@@ -92,30 +92,6 @@ pub type Executive = frame_executive::Executive<
 	AllPalletsReversedWithSystemFirst,
 >;
 
-pub const ONE_DAY: Moment = 86_400_000;
-
-pub const VERSION: RuntimeVersion = RuntimeVersion {
-	spec_name: create_runtime_str!("node-template"),
-	impl_name: create_runtime_str!("node-template"),
-	authoring_version: 1,
-	spec_version: 1,
-	impl_version: 1,
-	apis: RUNTIME_API_VERSIONS,
-	transaction_version: 1,
-	state_version: 0,
-};
-
-pub const MILLISECS_PER_BLOCK: u64 = 6000;
-
-pub const SLOT_DURATION: u64 = MILLISECS_PER_BLOCK;
-
-// Time is measured by number of blocks.
-pub const MINUTES: BlockNumber = 60_000 / (MILLISECS_PER_BLOCK as BlockNumber);
-pub const HOURS: BlockNumber = MINUTES * 60;
-pub const DAYS: BlockNumber = HOURS * 24;
-
-const NORMAL_DISPATCH_RATIO: Perbill = Perbill::from_percent(75);
-
 parameter_types! {
 	pub const Version: RuntimeVersion = VERSION;
 	pub const BlockHashCount: BlockNumber = 2400;
