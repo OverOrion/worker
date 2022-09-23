6d907ddc83e5c09ee690ab78ee89d0683a60b2bd
Szilard Parrag <szilard.parrag@gmail.com>
2022-09-23 16:05:33 +0200

    added mod opaque
    Signed-off-by: Szilard Parrag <szilard.parrag@gmail.com>

--- core-primitives/sgx-runtime-primitives/src/lib.rs
+++ core-primitives/sgx-runtime-primitives/src/lib.rs
@@ -8,3 +8,20 @@ mod tests {
 }
 
 pub mod types;
+
+/// Opaque types. These are used by the CLI to instantiate machinery that don't need to know
+/// the specifics of the runtime. They can then be made to be agnostic over specific formats
+/// of data like extrinsics, allowing for them to continue syncing the network through upgrades
+/// to even the core data structures.
+pub mod opaque {
+	use super::*;
+
+	pub use sp_runtime::OpaqueExtrinsic as UncheckedExtrinsic;
+
+	/// Opaque block header type.
+	pub type Header = generic::Header<BlockNumber, BlakeTwo256>;
+	/// Opaque block type.
+	pub type Block = generic::Block<Header, UncheckedExtrinsic>;
+	/// Opaque block identifier type.
+	pub type BlockId = generic::BlockId<Block>;
+}
