20aadb423b9d8618f5221beb8ec30afa71de158c
Szilard Parrag <szilard.parrag@gmail.com>
2022-11-06 23:46:16 +0100

    temporary rollback

--- enclave-runtime/src/tls_ra/tests.rs
+++ enclave-runtime/src/tls_ra/tests.rs
@@ -96,8 +96,7 @@ pub fn test_tls_ra_server_client_networking() {
 	thread::sleep(Duration::from_secs(1));
 
 	// Start client.
-	let stream = TcpStream::connect(SERVER_ADDR).unwrap();
-	let socket = TcpStream::from_stream(stream).unwrap();
+	let socket = TcpStream::connect(server_addr(port)).unwrap();
 	let result = request_state_provisioning_internal(
 		socket.as_raw_fd(),
 		SIGN_TYPE,
