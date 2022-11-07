0dc2498f12b073852d472963a3b0d4db5fdcf20d
Szilard Parrag <szilard.parrag@gmail.com>
2022-11-07 08:08:25 +0100

    clippy fixup for read_exact

--- enclave-runtime/src/tls_ra/tls_ra_client.rs
+++ enclave-runtime/src/tls_ra/tls_ra_client.rs
@@ -134,7 +134,7 @@ where
 		let opcode: Opcode = start_bytes[0].into();
 		// The 8 bytes following afterwards indicate the payload length.
 		let mut length_buffer = [0u8; 8];
-		self.tls_stream.read(&mut length_buffer)?;
+		self.tls_stream.read_exact(&mut length_buffer)?;
 		let payload_length = u64::from_be_bytes(length_buffer);
 		debug!("Payload length of {:?}: {}", opcode, payload_length);
 
@@ -144,7 +144,7 @@ where
 	/// Read all bytes into a buffer of given length.
 	fn read_until(&mut self, length: usize) -> EnclaveResult<Vec<u8>> {
 		let mut bytes = vec![0u8; length];
-		self.tls_stream.read(&mut bytes)?;
+		self.tls_stream.read_exact(&mut bytes)?;
 		Ok(bytes)
 	}
 }
