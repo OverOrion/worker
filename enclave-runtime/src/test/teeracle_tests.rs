32713f256469fa209785f6ccb1ecead6e44710a5
orion <szilard.parrag@gmail.com>
2023-02-06 12:50:12 +0100

    add debug logs for GHA action failure coin_gecko_result

--- enclave-runtime/src/test/teeracle_tests.rs
+++ enclave-runtime/src/test/teeracle_tests.rs
@@ -32,6 +32,7 @@ pub(super) fn test_verify_get_exchange_rate_from_coin_gecko_works() {
 	let coin_gecko_oracle = create_coin_gecko_oracle(Arc::new(MetricsOCallMock::default()));
 
 	let result = coin_gecko_oracle.get_exchange_rate(trading_pair.clone());
+	println("coin_gecko_oracle result is: {:#?}", &result);
 	assert!(result.is_ok());
 }
 
